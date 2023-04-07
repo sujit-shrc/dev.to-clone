7fe0854884ea26dffe8383fd5fc7e4527b61683a
Sujit Kumar <mnamesujit@gmail.com>
2023-04-07 19:37:25 +0530

    Added icons media and exported default from  constants dir

---
+++ src/constants/images.js
@@ -0,0 +1,34 @@
+import home from "../assets/home.png";
+import listing from "../assets/listing.png";
+import podcast from "../assets/podcast.png";
+import videos from "../assets/videos.png";
+import tags from "../assets/tags.png";
+import faqs from "../assets/faqs.png";
+import shop from "../assets/shop.png";
+import sponsors from "../assets/sponsors.png";
+import aboutdev from "../assets/about-dev.png";
+import contact from "../assets/contact.png";
+import guides from "../assets/guides.png";
+import comparison from "../assets/comparison.png";
+import thumb from "../assets/thumb.png";
+import privacy from "../assets/privacy.png";
+import eye from "../assets/eye.png";
+
+
+export default {
+    home,
+    listing,
+    podcast,
+    videos,
+    tags,
+    faqs,
+    shop,
+    sponsors,
+    aboutdev,
+    contact,
+    guides,
+    comparison,
+    thumb,
+    privacy,
+    eye,
+}
---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+import icons from "./images";
+
+export { icons };
